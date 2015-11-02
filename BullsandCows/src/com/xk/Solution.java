21ff8d2ca75022054b9a825cdb388a9e9c151f2f
xukan <xukan111@gmail.com>
2015-11-01 22:54:46 -0500

    bulls and cows

---
+++ BullsandCows/src/com/xk/Solution.java
@@ -0,0 +1,36 @@
+package com.xk;
+/*
+ * bull = number of elements that both value and index are equal.|| secret与guess下标与数值均相同的数字个数
+ * cow = number of elements that appear in secret but position is not correct||secret中有这个数字但是下标不对的数字的个数
+ * */
+public class Solution {
+	public static String getHint(String secret, String guess) {
+        int bulls=0, cows=0;
+        int count[] = new int[10];
+        StringBuilder res=new StringBuilder();
+        for(int i=0;i<secret.length();i++){
+            int s=secret.charAt(i)-'0', g =guess.charAt(i)-'0' ;
+            if(s==g)
+                bulls++;
+            else{
+                if(count[g]>0)
+                    cows++;
+                if(count[s]<0)
+                	cows++;
+                count[s]++;
+                count[g]--;
+            }
+        }
+        res.append(String.valueOf(bulls)).append("A").append(String.valueOf(cows)).append("B");
+        return res.toString();
+    }
+	
+	public static void main(String[] args) {
+//		 String secret ="1123";
+//		 String guess = "0111";
+		String secret ="1807";
+		 String guess = "7810";
+		 String res = getHint(secret, guess);
+		 System.out.println(res);
+	}
+}
--- MoveZeroes/src/com/xk/Solution.java
+++ MoveZeroes/src/com/xk/Solution.java
@@ -13,13 +13,16 @@ public class Solution {
 	            if (count != 0) {nums[i] = 0;}
 	        }
 	    }
+	    System.out.println(count);
         return nums;
     }
-	
+
 	public static void main(String[] args) {
-		int[] input={0, 1, 0, 3, 12};
+		int[] input={0, 0,1,3,5};
 		int[] res=moveZeroes(input);
 		for(int i: res)
 			System.out.println(i+" ");
 	}
+
 }
+ 
